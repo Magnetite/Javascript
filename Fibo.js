14ee0b70796ef1b2f190833e505f605c255330ba
Magnetite <spaceman389@yahoo.com>
2014-10-06 22:46:10 -0500

    Math Demos
    Two Math Demo functions added.
    FiboRatio shows that the ratio of successive terms in the fibonacci
    sequence approximate the Golden ratio.
    
    Sqrtof5 shows said ratios relationship to The Golden Ratio.

--- Fibo.js
+++ Fibo.js
@@ -9,5 +9,27 @@ function Fibo(a){
     return  fib;
 }
 
+//Calculates the Ratio between successive terms in the Fibonacci Sequence, Showing it Converges to the golden ratio.
+function FiboRatio(a){
+    var b = [0,1], fib = [];
+    for (var i = 0; i < a; i++){
+        b[i % 2] += b[(i+1) % 2];
+        
+        fib[i] = b[i % 2] / b[(i + 1) % 2];
+    }
+    return  fib;
+}
+
+// Related to the above function, as the Golden Ratio is equal to: ((Sqrt(5) - 1)/2
+function Sqrtof5(a){
+    var b = [0,1], fib = [];
+    for (var i = 0; i < a; i++){
+        b[i % 2] += b[(i+1) % 2];
+        
+        fib[i] = ( b[i % 2] / b[(i + 1) % 2] - 0.5) * 2;
+    }
+    return  fib;
+}
+
 
 Fibo(14);
