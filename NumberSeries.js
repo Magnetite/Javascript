1985ad4c630861fa69ba70c2a4e63287b51baa83
Magnetite <spaceman389@yahoo.com>
2014-10-09 18:14:34 -0500

    Added DeriveByPowerRule function.
    Calculates the derivative of a unnamed term, when given the coefficient,
    power, and what order derivative to find.

--- NumberSeries.js
+++ NumberSeries.js
@@ -13,5 +13,27 @@ function summer(a, c){
 }
 
 
-
-summer(31,7);
+//Calculates the derivative of a unnamed term, when given the coefficient, power, and what order derivative to find.
+function DeriveByPowerRule(coeff, power, deriv){
+    var out = [];
+    out[0] = 0;
+    
+    for (var i = 0; i < deriv; i++){
+        
+    if (deriv === 0){
+        return 1;
+    }
+    else if (deriv < 0) {
+        return false;
+    }
+    else {
+        coeff *= power;
+        power -= 1;
+        
+    }
+      
+    }
+    out[0] = coeff;
+    out[1] = power;
+    return out;
+}
