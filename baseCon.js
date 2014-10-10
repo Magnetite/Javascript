9e66f83896cdca89a45062302ea3f62c2eec4868
Magnetite <spaceman389@yahoo.com>
2014-10-10 17:15:14 -0500

    Added baseConvert, base 10 to ( 2 - 36 ).

--- baseCon.js
+++ baseCon.js
@@ -29,4 +29,33 @@ function baseCon(a, b){
     return out;
 }
 
-baseCon(50, 9);
+
+function baseConvert(a, b){
+    var tuo = [], out = [], i = 0;
+    tuo[0] = 0;
+    out[0] = 0;
+    var numerals = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f","g","h","i",
+    "j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    
+	//Check for proper usage
+	if (b < 2 || b > 36 || a < 1){
+	return false;
+	}
+    
+	//Converts from base 10 to base n.
+	//n is: n < 10 && n > 1
+    while (a > 0){
+        tuo[i] = numerals[a % b];
+        a /= b;
+        a = Math.floor(a);
+        i += 1;
+        
+    }
+    
+	//Reverses the output of last array
+	//to make it read from left to right when the array is output.
+    for (var j = 0, l = tuo.length; j < l; j++){
+        out[j] = tuo[ (l - j) - 1];
+    }
+    return out;
+}
