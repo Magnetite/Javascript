54fe1db6ae5fd34a6540cb0cb8cbdb770d80dc39
Magnetite <spaceman389@yahoo.com>
2017-05-18 23:47:53 -0500

    Added clear and id functions

--- StScript.js
+++ StScript.js
@@ -54,9 +54,16 @@ function classAdd(id, clss){
 	return true;
 }
 
-function Del(id){
-	document.getElementById(id)
+function clear(id){
+	id(id,"innerHTML = '' ");
+	return true;
 }
 
+function id(id, prop){
+	var doc =  document.getElementById(id);
+	doc += "." + prop;
+	eval(doc);
+	return true;
+}
 
 
