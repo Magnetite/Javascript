90da6e8668e590ccfc01857d3a01d3ceb3fb7c14
Magnetite <spaceman389@yahoo.com>
2017-05-24 23:55:50 -0500

    A web app for testing StScript library

---
+++ StScriptTester.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Calculator</title>
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="StScript.js"></script>
+
+<div id="output"></div>
+
+<table>
+<tr><td>1</td><td>2</td><td>3</td></tr>
+<tr><td>4</td><td>5</td><td>6</td></tr>
+<tr><td>7</td><td>8</td><td>9</td></tr>
+
+</table>
+
+
+
+</body>
+</html>
