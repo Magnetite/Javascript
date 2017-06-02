b095295af681d69152505ce93a9d8de73680b048
Magnetite <spaceman389@yahoo.com>
2017-06-01 23:24:30 -0500

    New tree function added,  (experimental)
    Experimenting with a array that can be used as a binary tree.

--- Tree.js
+++ Tree.js
@@ -15,3 +15,40 @@ function biTreeMake(arr){
     
 }
 
+
+
+
+//arrTree, an array that can be used as a tree
+//in a binary arrTree, a 1 represents a binary value based on it's index
+//and zero represents null. 
+
+function biArrTreeMake(mArr){
+    var outArr = [];
+    var tmp = 0;
+    var lastIndex = 0;
+    
+    for (var i = 0, len = mArr.length; i < len; i++){
+        for (var j = 0, lenJ = mArr[i].length; j < lenJ; j++){
+          tmp =  mArr[i][j];
+          
+          if (j === 0){
+              lastIndex = 1;
+          }
+          
+          lastIndex *= 2;
+          
+           if (tmp === 0){
+              lastIndex += 1;
+              
+          }
+          
+          outArr[lastIndex] = 1;
+          
+        }
+    }
+    
+    
+    return outArr;
+}
+
+
