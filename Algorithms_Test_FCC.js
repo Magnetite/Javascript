87aa431b823561ae21a59273231f3c08a1de9922
Magnetite <spaceman389@yahoo.com>
2017-06-06 23:46:27 -0500

    Successful Int to Roman Numeral Algorithm
    Got my function to convert ints to Roman numerals.

--- Algorithms_Test_FCC.js
+++ Algorithms_Test_FCC.js
@@ -1,18 +1,21 @@
 
+//Successful Algorithm!
 function convertToRoman(num) {
-  var romArr = ["M","D","C","L","X","V","I"];
-  var nums = [1000, 500, 100, 50, 10, 5, 1];
+  var romArr = ["M","CM", "D","CD", "C","XC","L","XL","X","IX","V", "IV","I"];
+  var nums = [1000, 900, 500, 400, 100, 90,   50, 40, 10,   9,   5,   4,  1];
   var ans = "";
+ 
   
   for (var i = 0, len = nums.length; i < len; i++){
     while (num >= nums[i]){
-      ans += romArr[i];
-      num -= nums[i];
+      
+     
+        ans += romArr[i];
+        num -= nums[i];
+      
+      
     }
   }
   
  return ans;
 }
-
-
-
