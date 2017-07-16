326cb942c80cda309590b3b5099bc67eaa6d2a15
Magnetite <spaceman389@yahoo.com>
2017-07-15 23:59:16 -0500

    Added Primes variable, and sumPrimes func.

--- MC.js
+++ MC.js
@@ -3,6 +3,8 @@
 
 var MC = {
 
+primes: [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127],
+
 loop: function(a, b){
 	for (var i = 0; i < b; i++){
 		//todo
@@ -11,6 +13,12 @@ loop: function(a, b){
 
 sum: function(arr){
 	return arr.reduce( function(a,b){return a + b; });
+},
+
+sumPrimes(a){
+	if (a % 2 === 0 ){
+		//todo
+	}
 }
 
 
