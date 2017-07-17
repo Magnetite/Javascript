50dacd786ce5abc8f0dba8bc58b01c83874b3f46
Magnetite <spaceman389@yahoo.com>
2017-07-16 23:57:01 -0500

    Added loop to find highest prime in array that is less than a.

--- MC.js
+++ MC.js
@@ -16,8 +16,23 @@ sum: function(arr){
 },
 
 sumPrimes(a){
+
+//An value of out1 is a prime that is added to a prime of out2 at the same index as out1.
+var out1 = [];  
+var out2 = [];
+
+var max_ind = 0; 
+var len = primes.length;
+
+	//find the index of array "primes" where the value is more than a, then subtract one from the index.
+	for (var j = 0; j < len || primes[j] > a; j++ ){ max_ind++; }
+	max_ind--;
+
 	if (a % 2 === 0 ){
-		//todo
+		for (var i = 0; i < len; i++){
+			
+		}
+		
 	}
 }
 
