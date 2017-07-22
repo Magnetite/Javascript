4abdbdfb94ed0286f24ed73424ec92f07e27e814
Magnetite <spaceman389@yahoo.com>
2017-07-21 23:59:27 -0500

    Added powerEnd method

--- MC.js
+++ MC.js
@@ -3,11 +3,13 @@
 
 var MC = {
 
-primes: [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127],
+primes: [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,
+         131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,229],
 
 loop: function(a, b){
 	for (var i = 0; i < b; i++){
 		//todo
+		a;
 	}
 },
 
@@ -40,7 +42,6 @@ squareEnd: function(a,b){
 	return (a * a) %  Math.pow(10, b);
 },
 
-numLength: function(a{
 numLength: function(a){
 	var i = 0;
 	for (; Math.pow(10,i) < a; i++){}
@@ -75,6 +76,15 @@ reverse: function(a){
 	
 	for ( var i = 0; i < len; i++, out *= 10){
 		out += MC.digitSelect(a, i);
+	}
+	
+	return out;
+},
+
+powerEnd: function(a, pow, digits){
+var out = 1;
+	for (var i = 0; i < pow; i++){
+		out = (out * a) % Math.pow(10, digits);
 	}
 	
 	return out;
