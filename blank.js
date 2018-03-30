9bcd778ffdc1259b9c8889e3f32cc81832d8604b
Steven <Magnetite@users.noreply.github.com>
2018-03-29 23:04:18 -0500

    A library to censor offensive words

---
+++ blank.js
@@ -0,0 +1,27 @@
+//blank.js by Steven Williams   
+//A library for blanking out offensive words on a webpage
+
+
+//The only Globals in this program
+var blockList = [];
+var replaceList = {};
+
+//Main program object
+var b = {
+
+    scan: function(ele){
+        //todo: to scan selected elements for words 
+    },
+
+    replace: function(){
+        //todo: replace is to replace a word, with a word from replaceList
+    },
+
+    asterisk: function(){
+        //todo: asterisk is to replace the middle letter with asterisks
+    },
+
+    del: function(ele){
+        //todo: to delete posts 
+    }
+}
